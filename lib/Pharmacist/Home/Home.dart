b2acd275201e96f5b10c0dfa0d2afeea36a61fb4
Abed Soudi <abedsoudi@yahoo.com>
2025-03-16 00:43:06 +0300

    finished PharmacistHome

--- lib/Pharmacist/Home/Home.dart
+++ lib/Pharmacist/Home/Home.dart
@@ -1,21 +1,6 @@
 // import '../../AuthLogic/AuthProvider.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const PharmacistHome(),
-    );
-  }
-}
 
 class PharmacistHome extends StatelessWidget {
   const PharmacistHome({super.key});
