7ca65c74b96b80fa067ec1dfae76252c8fdf9c37
Abed Soudi <abedsoudi@yahoo.com>
2025-03-22 20:20:09 +0300

    Pharmacist front done

--- lib/Pharmacist/Home/Home.dart
+++ lib/Pharmacist/Home/Home.dart
@@ -1,26 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+
 import 'package:prescripto/Pharmacist/Prescriptions/ReviewPrescription.dart';
-import 'dart:convert';
 
 import '../../data/database.dart';
 import 'HomeBackEnd.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const PharmacistHome(),
-    );
-  }
-}
 
 class PharmacistHome extends StatefulWidget {
   const PharmacistHome({super.key});
--- lib/Pharmacist/Inventory/Inventory.dart
+++ lib/Pharmacist/Inventory/Inventory.dart
@@ -1,20 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const InventoryScreen(),
-    );
-  }
-}
 
 class InventoryScreen extends StatelessWidget {
   const InventoryScreen({super.key});
